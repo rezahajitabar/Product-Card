5bc869ac2febebf098efcee766142ae512bac0e5
rezahajitabar <rezahajitabarchehre1996@gmail.com>
2024-03-05 10:21:12 +0330

    frist commit product card

---
+++ src/Product/Product.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./Product.css"
+function Product() {
+  return (
+    <div>
+        <h1>Product Card</h1>
+        <div className='card'>
+            <img src='logo192.png'/>
+            <h1>samsung A51</h1>
+            <p className='price'>$300</p>
+            <p>It is the best smart phone for 2021 It is the best smart phone for 2021</p>
+            <p><button>Add To Cart</button></p>
+        </div>
+    </div>
+  )
+}
+
+export default Product
