65d9844b5a02071bc2a402d71b00f52e8657d7e2
rezahajitabar <rezahajitabarchehre1996@gmail.com>
2024-03-06 10:47:52 +0330

    change code project

--- src/Product/Product.js
+++ src/Product/Product.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import "./Product.css"
-function Product(props) {
+function Product({image,title,price}) {
 
   
     return (
-      props.price > 90 && 
+    
       <div>
           <h2>Product Card</h2>
           <div className='card'>
-              <img src={props.image}/>
-              <h1>{props.title}</h1>
-              <p className='price'>${props.price}</p>
+              <img src={image}/>
+              <h1>{title}</h1>
+              <p className='price'>${price}</p>
               <p>It is the best smart phone for 2021 It is the best smart phone for 2021</p>
               <p><button>Add To Cart</button></p>
           </div>
