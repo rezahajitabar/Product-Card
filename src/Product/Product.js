1ea0022115afd4f7382a32e960541a14c839f4ff
rezahajitabar <rezahajitabarchehre1996@gmail.com>
2024-03-06 18:24:44 +0330

    commit change code project

--- src/Product/Product.js
+++ src/Product/Product.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import "./Product.css"
-function Product({image,title,price}) {
+function Product({image,title:ProductTitle,price}) {
 
-  
+  let title='Product Card'
     return (
     
       <div>
-          <h2>Product Card</h2>
+          <h2>{title}</h2>
           <div className='card'>
               <img src={image}/>
-              <h1>{title}</h1>
+              <h1>{ProductTitle}</h1>
               <p className='price'>${price}</p>
               <p>It is the best smart phone for 2021 It is the best smart phone for 2021</p>
               <p><button>Add To Cart</button></p>
