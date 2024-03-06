b926425e3ce882ceb1eda77274b6cf550a291896
rezahajitabar <rezahajitabarchehre1996@gmail.com>
2024-03-06 12:24:34 +0330

    two commit change project

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import './App.css';
 import Product from './Product/Product';
 import "./Application.css";
 function App() {
+
+  const allProducts=[
+    {id:1,title:'Asus',image:'images/asus.jpg',price:130},
+    {id:1,title:'Acur',image:'images/acur.jpg',price:190},
+    {id:1,title:'Samsung',image:'images/samsung.jpg',price:290}
+  ]
+
   return (
     <div className='container'>
-     <Product title="Asus" image="images/asus.jpg" price={130}/>
-     <Product title="Acer" image="images/acur.jpg" price={190}/>
-     <Product title="Samsung" image="images/samsung.jpg" price={290}/>
+     <Product {...allProducts[0]}/>
+     <Product {...allProducts[1]}/>
+     <Product {...allProducts[2]}/>
     </div>
   );
 }
